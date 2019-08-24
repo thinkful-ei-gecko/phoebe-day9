b5205b1492f1ba1b6295d5c7750cf79ec41decc3
Phoebe Law <phoebebasilio@gmail.com>
2019-08-24 15:57:29 -0700

    Created ability to access sub-breeds. Edited CSS formatting. Prevent images to load to DOM if error.

--- index.js
+++ index.js
@@ -1,28 +1,98 @@
 'use strict';
 
-//function to display results/printing to console
-function displayResults(responseJson) {
-  console.log(responseJson.message);
-  $('.results').empty();
+function displayPhotoResults(responseJson) {
+  console.log('`displayPhotoResults` started to run');
   let img = responseJson.message;
-  $('.results').append(`<img src='${img}'>`);
+  $('.js-photo-results').html(`<img src='${img}'>`);
 }
 
-//function to GET the images
-function fetchDogImage(breedType) {
-  fetch(`https://dog.ceo/api/breed/${breedType}/images/random`)
+//function to display results/printing to console
+function displayBreedResults(breedType, responseJson) {
+  console.log('`displayBreedResults` started to run');
+  if (responseJson.hasOwnProperty('code')) {
+    console.log('`displayBreedResults`: (is an error) started to run');
+    $('.js-breed-info').html(`
+    <p>Sorry, we can't find any results for "<span class="breed-type">${breedType}</span>". Please check for typos and try again.</p>
+    <p>If you're looking for a sub-breed, try searching the master breed to see its available photos by sub-breed.</p>
+    `);
+  }
+  else {
+    if (responseJson.message.length === 0 || responseJson.message.length === 1) {
+      console.log('`displayBreedResults`: (has no subbreeds, going to fetch) started to run');
+      fetchDogImage(breedType);
+    } 
+    else {
+      console.log('`displayBreedResults`: (getting subbreed results) started to run');
+      $('.js-breed-info').html(`
+        <p>We found the following results for "${breedType}". Please click on one of the following results to display an image for the ${breedType} breed.</p>
+      `);
+      $('.js-breed-results').append(`
+        <input class="${breedType}" id="main" type="button" value="${breedType}, all">
+      `);
+      responseJson.message.forEach(item => $('.js-breed-results').append(`
+        <input class="${breedType}" id="${item}" type="button" value="${item} ${breedType}">
+      `));
+      breedListener();
+    }
+  }
+}
+
+function fetchDogImage(breedType, subBreed) {
+  console.log('`fetchDogImage` started to run');
+  if (subBreed) {
+    fetch (`https://dog.ceo/api/breed/${breedType}/${subBreed}/images/random`)
+      .then(response => response.json())
+      .then(responseJson => displayPhotoResults(responseJson));
+    //probably need to account for an error here
+    // .catch(error => alert('Something went wrong. Please try again later.'));
+  }
+  else {
+    fetch(`https://dog.ceo/api/breed/${breedType}/images/random`)
+      .then(response => response.json())
+      .then(responseJson => displayPhotoResults(responseJson));
+  }
+}
+
+function clearResultsFields() {
+  console.log('`clearResultsFields` started to run');
+  $('.js-breed-info').empty();
+  $('.js-breed-results').empty();
+  $('.js-photo-results').empty();
+}
+
+function fetchDogBreeds(breedType) {
+  console.log('`fetchDogBreeds` started to run');
+  fetch(`https://dog.ceo/api/breed/${breedType}/list`)
     .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson))
-    .catch(error => alert('That is not a recognized breed type.'));
+    .then(responseJson => displayBreedResults(breedType, responseJson));
+}
+
+function breedListener() {
+  console.log('`breedListener` started to run');
+  $('.js-breed-results').on('click', e => {
+    console.log('breedListener clicked');
+    let subBreed = e.target.getAttribute('id');
+    let breedType = e.target.getAttribute('class');
+    if (subBreed === 'main') {
+      fetchDogImage(breedType);
+    }
+    else {
+      fetchDogImage(breedType, subBreed);
+    }
+    //do something if there's an error here
+  });
 }
 
 //listener on the submit button 
 function clickListener() {
-  $('form').submit(event => {
+  console.log('`clickListener` started to run');
+  $('.dog-pic-form').submit(event => {
     event.preventDefault();
+    //not sure if the below is right
     let breedType = $('#breed-input').val();
-    fetchDogImage(breedType);
+    clearResultsFields();
+    console.log(breedType);
+    fetchDogBreeds(breedType);
   });
 }
 
